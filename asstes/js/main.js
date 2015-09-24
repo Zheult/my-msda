6d5ea5e2e65c3c7e972c82e3395ebc57a96fa2e3
Zheult <zheult@gmail.com>
2015-09-24 18:29:38 +0400

    todays last commit

--- asstes/js/main.js
+++ asstes/js/main.js
@@ -5,39 +5,58 @@
         var $window = $(window),
             $body = $('body');
 
+        var lightSlider, refreshLightSlider;
+
         var $lightSlider = $('#lightSlider');
         if ($lightSlider.length) {
-            var lsOptions = {item: 1, pager: false},
-                lightSlider = $lightSlider.lightSlider(lsOptions);
+            lightSlider = $lightSlider.lightSlider({item: 1, pager: false});
 
-            var _timeout;
-            var refreshLightSlider = function (t) {
+            var _lightSliderTimeout;
+            refreshLightSlider = function (t) {
                 t = t || 450;
                 lightSlider.hide();
-                clearTimeout(_timeout);
-                _timeout = setTimeout(function () {
+                clearTimeout(_lightSliderTimeout);
+                _lightSliderTimeout = setTimeout(function () {
                     lightSlider.show().refresh();
                 }, t);
             };
-
-            $('.sidebar-toggle').on('click', function () {
-                $body.toggleClass('sidebar-collapsed');
-                refreshLightSlider();
-            });
-
-            $window.on('resize', function () {
-                var sidebarCollapsed = $body.hasClass('sidebar-collapsed');
-                if ($window.width() < 1200) {
-                    if (!sidebarCollapsed) $body.addClass('sidebar-collapsed');
-                } else if (sidebarCollapsed) {
-                    $body.removeClass('sidebar-collapsed');
-                }
-                refreshLightSlider(800);
-            });
-        } else {
-            $('.sidebar-toggle').on('click', function () {
-                $body.toggleClass('sidebar-collapsed');
-            });
         }
+
+        var _equalizerTimeout;
+        var equalizeHeights = function (t) {
+            var equalize = function () {
+                var maxHeight = 0;
+                $('.equalize').css('height', '').each(function () {
+                    var parentHeight = $(this).parent().height();
+                    if (parentHeight > maxHeight) maxHeight = parentHeight;
+                }).height(maxHeight);
+            };
+            t = typeof t !== 'number' ? 400 : t;
+            if (t > 0) {
+                clearTimeout(_equalizerTimeout);
+                _equalizerTimeout = setTimeout(equalize, t);
+            } else {
+                equalize();
+            }
+        };
+
+        equalizeHeights(0);
+
+        $window.on('resize', function () {
+            equalizeHeights();
+            var sidebarCollapsed = $body.hasClass('sidebar-collapsed');
+            if ($window.width() < 1200) {
+                if (!sidebarCollapsed) $body.addClass('sidebar-collapsed');
+            } else if (sidebarCollapsed) {
+                $body.removeClass('sidebar-collapsed');
+            }
+            if (typeof refreshLightSlider === 'function') refreshLightSlider();
+        }).resize();
+
+        $('.sidebar-toggle').on('click', function () {
+            equalizeHeights();
+            $body.toggleClass('sidebar-collapsed');
+            if (typeof refreshLightSlider === 'function') refreshLightSlider();
+        });
     });
 })(jQuery, window);
